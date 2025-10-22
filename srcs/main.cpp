e3d4b3cee57f9798fb62b725a98beadb26e01438
jose5556 <josefilmeneses@gmail.com>
2025-10-22 13:11:18 +0100

    test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,13 +3,22 @@
 int main() {
 
     I2c::All_init();
+
+
     I2c::set_servo_angle(150);
     I2c::motor(0,50,1);
+
+
     sleep(3);
-    I2c::set_servo_angle(90);
     I2c::stop_motors();
-	I2c::motor(1,50,1);
+
+    I2c::set_servo_angle(90);
+	I2c::motor(0,50,0);
+
+
     sleep(3);
+
+
     I2c::brake_motor();
     return 0;
 }
