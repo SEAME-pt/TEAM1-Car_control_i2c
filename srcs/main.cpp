5ca0a988cca03beb60440d75affaa4b12b9ad80d
jose5556 <josefilmeneses@gmail.com>
2025-10-24 12:05:19 +0100

    proper main for tests

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -5,19 +5,27 @@
 int main() {
 	
 	I2c::init(0x60,0x40,"/dev/i2c-1");
-	I2c::set_servo_angle(150);
+	I2c::set_servo_angle(60);
 	I2c::motor(0,50,1);
 	sleep(3);
 
 	I2c::stop_motors();
-	I2c::set_servo_angle(90);
+	I2c::set_servo_angle(120);
 	I2c::motor(0,50,1);
+	sleep(3);
+
+	I2c::stop_motors();
+	I2c::set_servo_angle(60);
+	I2c::motor(0,50,0);
 	sleep(3);
 
 	I2c::stop_motors();
 	I2c::set_servo_angle(0);
 	I2c::motor(0,50,0);
 	sleep(3);
+
+	I2c::set_servo_angle(60);
+	I2c::motor(0,50,0);
 
 	I2c::stop_motors();
 }
