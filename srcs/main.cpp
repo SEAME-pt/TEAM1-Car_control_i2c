e722ecc901dafab9cbfb90881afd4584a98a2584
therappha <therappha@gmail.com>
2025-10-24 15:12:31 +0100

    feature: car control with joystick first test'

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -39,9 +39,20 @@
 #include <iostream>
 #include "../lib/sdl.h"
 
+double mapAxisToAngle(double axisValue, double angleMin, double angleMax, double angleCenter) {
+    if (axisValue < 0) {
+        // Map [-1, 0] -> [angleMin, angleCenter]
+        return ((angleCenter - angleMin) * (axisValue - (-1)) / (0 - (-1)) + angleMin);
+    } else {
+        // Map [0, 1] -> [angleCenter, angleMax]
+        return ((angleMax - angleCenter) * (axisValue - 0) / (1 - 0) + angleCenter);
+    }
+}
 
-int main() {
-	
+int main() 
+{
+	int steering = 60;
+	int throttle = 0;
 	if (SDL_Init(SDL_INIT_JOYSTICK) < 0) {
 		std::cerr << "ERROR! SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
 		return (1);
@@ -51,7 +62,8 @@ int main() {
 		std::cerr << "ERROR! No Joysticks detected: " << SDL_GetError() << std::endl;
 		return (1);
 	}
-
+	I2c::init(0x60,0x40,"/dev/i2c-1");
+	I2c::set_servo_angle(steering);
 	SDL_Joystick *joystick = SDL_JoystickOpen(index_0_controller);
 	if (joystick == nullptr) {
 		std::cerr << "ERROR! Failed to open joystick: " << SDL_GetError() << std::endl;
@@ -71,12 +83,43 @@ int main() {
 	} */
 
 	SDL_Event e;
-	while (true) {
-		SDL_PollEvent(&e);
+	while (true) 
+	{
+
 		float axis0 = SDL_JoystickGetAxis(joystick, 2) / 32767.0f; // steering
 		float axis1 = SDL_JoystickGetAxis(joystick, 1) / 32767.0f; // throttle
-		std::cout << "Steering: " << axis0 << " Throttle: " << -axis1 << "\r";
-		SDL_Delay(50);
+
+		steering = static_cast<int>(mapAxisToAngle(axis0, 0, 120, 60));
+		throttle = static_cast<int>(mapAxisToAngle(axis1, -100, 100, 0));
+
+		I2c::set_servo_angle(steering);
+		
+		if (throttle > 0) {
+			I2c::motor(0, throttle, 1); // Forward
+		} else if (throttle < 0) {
+			I2c::motor(0, -throttle, 0); // Backward
+		} else {
+			I2c::stop_motors(); // Stop
+		}
+		while (SDL_PollEvent(&e))
+		{
+			switch (e.type)
+			{
+				case SDL_JOYBUTTONDOWN:
+					if (e.jbutton.button == 11) // Assuming button 0 is the 'A' button
+					{
+						std::cout << "Button A pressed. Exiting...\n";
+						SDL_JoystickClose(joystick);
+						I2c::stop_all(); 
+						SDL_Quit();
+						return 0;
+					}
+					break;
+			}
+		}
+		//std::cout << "Steering PWM: " << steering << " Throttle PWM: " << -throttle << std::endl;
+		
+		SDL_Delay(10);
 	}
 
 	SDL_JoystickClose(joystick);
